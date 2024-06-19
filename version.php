ef7c2d747998f0210c70bc8d144e48d9b7305a82
Harshil Patel <harshil8595@gmail.com>
2024-06-19 23:06:44 +0530

    Bump version

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023010907;
+$plugin->version   = 2024061900;
 $plugin->requires  = 2022041900;
 $plugin->supported = [400, 404];
 $plugin->component = 'block_timezoneclock';
