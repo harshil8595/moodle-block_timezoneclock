f2e9dd0ae686d1b47a0c00ac681e00541727983c
Harshil Patel <harshil8595@gmail.com>
2024-06-19 22:37:09 +0530

    Make Code checker happy

--- settings.php
+++ settings.php
@@ -13,6 +13,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
 /**
  * Block timezoneclock settings.
  * @package   block_timezoneclock
