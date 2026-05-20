1977b3c515242a66027b5618c9e51a8673686948
Avery_Bowenlearn <avery@bowenlearn.com>
2026-05-20 14:43:09 -0400

    Fixes a bug forcing login if not currently logged in

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026040500;
+$plugin->version   = 2026040501;
 $plugin->requires  = 2022041900;
 $plugin->supported = [400, 502];
 $plugin->component = 'block_timezoneclock';
