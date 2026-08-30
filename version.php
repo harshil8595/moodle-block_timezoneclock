f45edf4c4b95d33fdbdab8d79ecafba0eb8b402c
Harshil Patel <harshil8595@gmail.com>
2026-08-30 13:52:26 +0000

    Version release

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026062100;
+$plugin->version   = 2026083000;
 $plugin->requires  = 2022041900;
 $plugin->supported = [400, 502];
 $plugin->component = 'block_timezoneclock';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v1.2.3';
+$plugin->release = 'v1.2.4';
